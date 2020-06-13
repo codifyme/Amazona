8855ba09b4a0d2d75b113b9857225a376766331f
Angel Cochachez <acochachez@gmail.com>
2020-06-13 14:22:43 -0700

    Created a React App and Render products

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,31 @@
+export default data={
+    products:[
+        {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/shirt1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/shirt1.jpg',
+        price:50,
+        brand:'Nike',
+        rating:4.2,
+        numReviews:9
+    },
+    {
+        name:'Best Pants',
+        category:'Pants',
+        image:'/images/shirt1.jpg',
+        price:70,
+        brand:'Nike',
+        rating:4.6,
+        numReviews:8
+    },
+]
+}
