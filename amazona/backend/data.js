3ed71248a19d2c889a0b7483468578d72e8e2a50
Angel Cochachez <acochachez@gmail.com>
2020-06-15 18:34:18 -0700

    Added backend files server and data js

---
+++ amazona/backend/data.js
@@ -0,0 +1,34 @@
+export default data={
+    products:[
+        {
+        _id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/shirt1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'2',
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/shirt1.jpg',
+        price:50,
+        brand:'Nike',
+        rating:4.2,
+        numReviews:9
+    },
+    {
+        _id:'3',
+        name:'Best Pants',
+        category:'Pants',
+        image:'/images/shirt1.jpg',
+        price:70,
+        brand:'Nike',
+        rating:4.6,
+        numReviews:8
+    },
+]
+}
