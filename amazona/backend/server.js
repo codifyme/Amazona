64e1671e6adbf9eca58cd0ae07d905f8758460de
Angel Cochachez <acochachez@gmail.com>
2020-06-19 12:53:23 -0700

    Parsing server using express and the parse-serve module for the backend

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import data from './data';
 
+const app = express();
 
-const app=express();
-
-app.get("/api/products", (req.res) => {
-    res.send(data.products);
+app.get('/api/products', (req, res) => {
+	res.send(data.products);
 });
 
-app.listen(5000, ()=>{console.log("Server started at http://localhost:5000")});
+app.listen(5000, () => {
+	console.log('Server started at http://localhost:5000');
+});
--- amazona/frontend/src/data.js
+++ amazona/frontend/src/data.js
@@ -1,4 +1,4 @@
-export default data={
+export default data = {
     products:[
         {
         _id:'1',
