293e9efb3dd998f3aa6e60328a5ab2d2ccff55ad
Angel Cochachez <acochachez@gmail.com>
2020-06-24 11:38:54 -0700

    changed PORT Environment variable

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -8,12 +8,19 @@ const app = express();
 // });
 
 app.get('/', (req, res)=>{
-	res.send('Hello World mi amigo');
+	res.send('Hola amigoss!!!!');
 });
 app.get('/api/courses', (req,res)=>{
 	res.send([1,2,3]);
 });
 
-app.listen(5000, () => {
-	console.log("Server started at http://localhost:5000");
+app.get('/api/courses/:id', (req,res)=>{
+	res.send(req.params.id);
 });
+
+//PORT ENVIROMENT VARIABLE
+const port=process.env.PORT ||5000;
+app.listen(port, () => 
+	// console.log("Server started at http://localhost:5000");
+	console.log(`Listening on port ${port} ...`)
+);
