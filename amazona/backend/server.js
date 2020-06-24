6688c9d6853bf952a6b60151167271d6def64a10
Angel Cochachez <acochachez@gmail.com>
2020-06-23 17:17:14 -0700

    testing server with API routes and data

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -3,8 +3,15 @@ import data from './data';
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-	res.send(data.products);
+// app.get('/api/products', (req, res) => {
+// 	res.send[(data.products)];
+// });
+
+app.get('/', (req, res)=>{
+	res.send('Hello World mi amigo');
+});
+app.get('/api/courses', (req,res)=>{
+	res.send([1,2,3]);
 });
 
 app.listen(5000, () => {
